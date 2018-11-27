cfa87f401832106f543176b0c81248b81482621f
yaqingzhang <zhang_yaqing@sina.cn>
2018-11-27 18:47:03 +0800

    [Channels] 提交 通道

--- dev-kotref/app/src/main/java/com/runningmessage/kotref/MainActivity.kt
+++ dev-kotref/app/src/main/java/com/runningmessage/kotref/MainActivity.kt
@@ -41,10 +41,10 @@ class MainActivity : AppCompatActivity() {
         // Set up the ViewPager with the sections adapter.
         container.adapter = mSectionsPagerAdapter
 
-        fab.setOnClickListener { view ->
+/*        fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
-        }
+        }*/
 
     }
 
@@ -162,6 +162,7 @@ class MainActivity : AppCompatActivity() {
                     map1["Channels.Companion.t07"] = Channels.Companion::t07
                     map1["Channels.Companion.t08"] = Channels.Companion::t08
                     map1["Channels.Companion.t09"] = Channels.Companion::t09
+                    map1["Channels.Companion.t10"] = Channels.Companion::t10
 
 
                     val list = ArrayList<LinkedHashMap<String, () -> Any>>()
--- dev-kotref/app/src/main/java/com/runningmessage/kotref/kotlin/coroutines/Channels.kt
+++ dev-kotref/app/src/main/java/com/runningmessage/kotref/kotlin/coroutines/Channels.kt
@@ -242,6 +242,52 @@ class Channels {
 
                 }
             }
+            /**[player]*/
+        }
+
+
+        /**计时器通道*/
+        fun t10() = wrap {
+
+            runBlocking {
+
+                val tickerChannel = ticker(delayMillis = 100, initialDelayMillis = 0)
+
+                var nextElement = withTimeoutOrNull(1) {
+                    tickerChannel.receive()
+                }
+
+                mPrintln("Initial element is available immediately: $nextElement")
+
+                nextElement = withTimeoutOrNull(50) {
+                    tickerChannel.receive()
+                }//	所有随后到来的元素都经过了100毫秒的延迟
+
+                mPrintln("Next element is not ready in 50 ms: $nextElement")
+
+                nextElement = withTimeoutOrNull(60) {
+                    tickerChannel.receive()
+                }
+
+                mPrintln("Next element is ready in 100 ms: $nextElement")
+
+                //模拟大量消费延迟
+                mPrintln("Consumer pause for 150ms")
+                delay(150)
+
+                //下一个元素立即可用 :: 因为间隔已经大于100ms
+                nextElement = withTimeoutOrNull(1) { tickerChannel.receive() }
+                println("Next element is available immediately after large consumer delay: $nextElement")
+
+                //	请注意,`receive`	调用之间的暂停被考虑在内,下一个元素的到达速度更快
+                nextElement = withTimeoutOrNull(60) {
+                    tickerChannel.receive()
+                }
+                mPrintln("Next element is ready in 50ms after consumer pause in 150ms: $nextElement")
+
+                tickerChannel.cancel()
+            }
+            /**[t10]*/
         }
 
     }
