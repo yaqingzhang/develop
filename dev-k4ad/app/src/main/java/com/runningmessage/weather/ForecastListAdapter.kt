4f84174952b1b329ea02b22b42ba3143edf8ea3d
yaqingzhang <zhang_yaqing@sina.cn>
2018-12-14 18:38:57 +0800

    [Page106]

--- dev-k4ad/app/src/main/java/com/runningmessage/weather/ForecastListAdapter.kt
+++ dev-k4ad/app/src/main/java/com/runningmessage/weather/ForecastListAdapter.kt
@@ -5,12 +5,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.runningmessage.weather.domain.model.Forecast
 import com.runningmessage.weather.domain.model.ForecastList
 import com.runningmessage.weather.utils.ctx
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_forecast.view.*
 
 /**
  * Created by Lorss on 18-12-5.
@@ -35,21 +34,15 @@ class ForecastListAdapter(private val items: ForecastList, val itemClick: (Forec
 
     class ViewHolder(val view: View, val itemClick: (Forecast) -> Unit) : RecyclerView.ViewHolder(view) {
 
-        private val iconView: ImageView = view.findViewById(R.id.icon)
-        private val dateView: TextView = this.view.findViewById(R.id.date)
-        private val descriptionView: TextView = view.findViewById(R.id.description)
-        private val maxTemperatureView: TextView = view.findViewById(R.id.maxTemperature)
-        private val minTemperatureView: TextView = view.findViewById(R.id.minTemperature)
-
         fun bindForecast(forecast: Forecast) {
             with(forecast) {
 
-                Picasso.get().load(iconUrl).into(iconView)
+                Picasso.get().load(iconUrl).into(view.icon)
 
-                dateView.text = date
-                descriptionView.text = description
-                maxTemperatureView.text = high.toString()
-                minTemperatureView.text = low.toString()
+                itemView.date.text = date
+                itemView.description.text = description
+                itemView.maxTemperature.text = high.toString()
+                itemView.minTemperature.text = low.toString()
                 itemView.setOnClickListener { itemClick(forecast) }
             }
 
